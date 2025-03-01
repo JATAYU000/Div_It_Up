4a2546b7e8f9389bf9a600fa465c2b74a516fa6a
Rohit Reddy <5668.rohit@gmail.com>
2025-03-01 12:14:16 +0530

    Updated code

--- bank-solution/app.js
+++ bank-solution/app.js
@@ -1,13 +1,47 @@
-function updateRoute(templateId) {
-    const template = document.getElementById(templateId);
+const routes = {
+    '/login': { 
+        templateId: 'login' ,
+        title: 'Login Page'
+    },
+    '/dashboard': { 
+        templateId: 'dashboard',
+        title: 'Dashboard'
+    },
+    '/credits': {
+        templateId: 'credits',
+        title: 'credits'
+    },
+};
+
+function updateRoute() {
+    const path = window.location.pathname;
+    const route = routes[path];
+    
+    if (!route) {
+        return navigate('/login');
+      }
+
+    const template = document.getElementById(route.templateId);
     const view = template.content.cloneNode(true);
     const app = document.getElementById('app');
+    document.title = route.title;
+    console.log(`${route.title} is shown`);
     app.innerHTML = '';
     app.appendChild(view);
   }
-updateRoute('Login');
 
-const routes = {
-    '/login': { templateId: 'login' },
-    '/dashboard': { templateId: 'dashboard' },
-};
+
+updateRoute('login');
+
+function navigate(path) {
+  window.history.pushState({}, path, path);
+  updateRoute();
+}
+
+function onLinkClick(event) {
+    event.preventDefault();
+    navigate(event.target.href);
+  }
+
+window.onpopstate = () => updateRoute();
+updateRoute();
